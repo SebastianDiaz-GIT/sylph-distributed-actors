893993dfcc0ab7a4e07daebd248a87d69217b415
Sebastian Diaz <sdiaz@softcaribbean.com>
2025-12-24 11:11:02 -0500

    Agrega método poll() a la interfaz Mailbox
    Implementa un método no bloqueante para obtener mensajes de la mailbox.
    Esto permite verificar si hay mensajes disponibles sin bloquear el hilo actual.

--- src/main/java/sylph/interfaces/mailbox/Mailbox.java
+++ src/main/java/sylph/interfaces/mailbox/Mailbox.java
@@ -5,5 +5,8 @@ import sylph.interfaces.message.Message;
 public interface Mailbox {
     void put(Message message) throws InterruptedException;
     Message take() throws InterruptedException;
+    /**
+     * Non-blocking poll of the mailbox. Returns null if empty.
+     */
+    Message poll();
 }
-
